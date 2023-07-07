0d90585283cd747ce8852008f3dbc3a2722e7428
Lucas Tavares <lucas.tavares@poli.ufrj.br>
2023-07-06 22:50:34 -0300

    Updates

---
+++ precache-manifest.bdf219a7a7d17533bd6f2bdbca7439bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7361e7efd7538e7f31494bf1cbaa1a",
+    "url": "/emd-desafio-frontend/index.html"
+  },
+  {
+    "revision": "b981c561cf7ddc4e79a2",
+    "url": "/emd-desafio-frontend/static/css/main.bdba8336.chunk.css"
+  },
+  {
+    "revision": "f7c6b5e403e932c8d978",
+    "url": "/emd-desafio-frontend/static/js/2.d863c905.chunk.js"
+  },
+  {
+    "revision": "b981c561cf7ddc4e79a2",
+    "url": "/emd-desafio-frontend/static/js/main.0b581802.chunk.js"
+  },
+  {
+    "revision": "f960bfefb0f6cd9fc051",
+    "url": "/emd-desafio-frontend/static/js/runtime~main.a07a5589.js"
+  }
+]);
